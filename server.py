64ef095f1cf2e7ad0e30ee6971d248d7040b668e
guigaoliveira <gmbo182@hotmail.com>
2019-11-06 00:06:55 -0300

    server quase ok

--- server.py
+++ server.py
@@ -1,54 +1,83 @@
 import socket
 from threading import Thread
+import re
 
+def int_to_bytes(val, num_bytes):
+    return [(val & (0xff << pos*8)) >> pos*8 for pos in range(num_bytes)]
 
 def bytesFormat(data):
     return bytes(data, "utf-8")
+
+
+def getKeyByValue(dict, value):
+    return list(dict.keys())[list(dict.values()).index(value)]
+
+
+def get_client_list(clients):
+    result = ""
+    for client, name in clients.items():
+        host, port = client.getpeername()
+        result += f"<{name}, {host}, {port}>\n"
+    return result[:-1]
+
+
+def broadcast(msg, prefix=""):  # prefix is for name identification.
+    """Broadcasts a message to all the clients."""
+    for sock in clients:
+        sock.send(bytesFormat(prefix) + msg)
+
+
+def handle_exit_request(client):
+    client.close()
+    name = clients[client]
+    del clients[client]
+    broadcast(bytesFormat(f"{name} saiu!"))
+
+
+def handle_private_message(msg):
+    match = re.search(r"privado\((\w+)\)", msg, re.IGNORECASE)
+    if match:
+        client_name = match.group(1)
+        return getKeyByValue(clients, client_name)
+    return None
 
 
 def handle_connections():
     """Sets up handling for incoming clients."""
     while True:
         client, client_address = SERVER.accept()
-        print("%s:%s has connected." % client_address)
-        client.send(bytesFormat(
-            "Greetings from the cave! Now type your name and press enter!"))
-        addresses[client] = client_address
+        print("%s:%s está conectado." % client_address)
+        client.send(bytesFormat("Digite seu nickname."))
         Thread(target=handle_messages, args=(client,)).start()
 
 
 def handle_messages(client):  # Takes client socket as argument.
     """Handles a single client connection."""
-
     name = client.recv(BUFSIZ).decode("utf8")
-    welcome = 'Welcome %s! If you ever want to quit, type {quit} to exit.' % name
-    client.send(bytesFormat(welcome))
-    msg = "%s has joined the chat!" % name
-    broadcast(bytesFormat(msg))
+    client.send(bytesFormat(
+        f"Seja bem vindo {name}! Para sair do chat, digite sair()."))
+    broadcast(bytesFormat(f"{name} entrou..."))
     clients[client] = name
 
     while True:
         msg = client.recv(BUFSIZ)
+        msgStr = msg.decode("utf-8")
         print(msg)
-        if msg != bytesFormat("{quit}"):
+        if msgStr != "sair()":
+            if msgStr == "lista()":
+                client.send(bytesFormat(get_client_list(clients)))
+                break
+            private_client = handle_private_message(msgStr)
+            if private_client:
+                private_client.send(bytesFormat("te mandaram um privado lek"))
+                break
             broadcast(msg, name+": ")
         else:
-            client.send(bytesFormat("{quit}"))
-            client.close()
-            del clients[client]
-            broadcast(bytesFormat(f"{name} has left the chat."))
+            handle_exit_request(client)
             break
 
 
-def broadcast(msg, prefix=""):  # prefix is for name identification.
-    """Broadcasts a message to all the clients."""
-
-    for sock in clients:
-        sock.send(bytesFormat(prefix) + msg)
-
-
 clients = {}
-addresses = {}
 
 HOST = ''
 PORT = 9001
@@ -62,7 +91,15 @@ SERVER.bind(ADDR)
 if __name__ == "__main__":
     SERVER.listen(5)
     print("Waiting for connection...")
+    print(int_to_bytes(5, 2))
     ACCEPT_THREAD = Thread(target=handle_connections)
     ACCEPT_THREAD.start()
+
+    msg = input()
+    while True:
+        if(msg == "lista()"):
+            print(get_client_list(clients))
+        msg = input()
+
     ACCEPT_THREAD.join()
     SERVER.close()
